01c840523badecaa42982344797919bf6eb787de
Vikram Jain <vjain40@gatech.edu>
2016-06-03 02:08:26 -0400

    base code

---
+++ app/src/main/java/vikram/com/parkenhance/ParkLayoutActivity.java
@@ -0,0 +1,13 @@
+package vikram.com.parkenhance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParkLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.park_layout_main);
+    }
+}
